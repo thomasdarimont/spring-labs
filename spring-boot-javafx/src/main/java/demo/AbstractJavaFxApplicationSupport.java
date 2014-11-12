606fd4662b82a32632c99157b16f20258264013a
Hendrik Ebbers <hendrik.ebbers@web.de>
2014-11-12 23:54:07 +0100

    Simplified Bootstrapping.
    Instead of registering the Application as a bean itself, we just let the spring container autowire it’s dependencies.
    Added idea artifacts to gitignore.
    
    Original pull request: #1.

--- spring-boot-javafx/src/main/java/demo/AbstractJavaFxApplicationSupport.java
+++ spring-boot-javafx/src/main/java/demo/AbstractJavaFxApplicationSupport.java
@@ -1,13 +1,10 @@
 package demo;
 
 import javafx.application.Application;
-
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
-
-import com.sun.xml.internal.ws.util.StringUtils;
 
 /**
  * @author Thomas Darimont
@@ -19,28 +16,26 @@ public abstract class AbstractJavaFxApplicationSupport extends Application {
 	private final ConfigurableApplicationContext applicationContext;
 
 	public AbstractJavaFxApplicationSupport() {
-
+		
 		Import configClass = getClass().getAnnotation(Import.class);
+		Class<?>[] configClasses = configClass.value();
 
-		Object[] configClasses = (Object[]) configClass.value();
 		applicationContext = SpringApplication.run(configClasses, savedArgs);
 
 		AutowireCapableBeanFactory bf = applicationContext.getAutowireCapableBeanFactory();
 		bf.autowireBean(this);
-
-		applicationContext.getBeanFactory().registerSingleton(StringUtils.decapitalize(getClass().getSimpleName()), this);
 	}
 
 	@Override
 	public void stop() throws Exception {
+		
 		super.stop();
 		applicationContext.close();
 	}
 
-	protected static void launchApp(Class<? extends Application> appClass, String[] args) {
-
+	protected static void launchApp(Class<? extends AbstractJavaFxApplicationSupport> appClass, String[] args) {
+		
 		AbstractJavaFxApplicationSupport.savedArgs = args;
-
 		Application.launch(appClass, args);
 	}
 }
